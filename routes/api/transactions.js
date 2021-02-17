519dfb1bf9363f9ada100512e91225feff9b4a92
nancyma713 <nancyma713@outlook.com>
2021-02-17 00:54:00 -0500

    add transactions routes

--- routes/api/transactions.js
+++ routes/api/transactions.js
@@ -11,4 +11,46 @@ router.get('/', (req, res) => {
         .catch(err => res.status(404).json({ notransactionfound: 'No transaction found' }));
 });
 
+router.get('/:userId', (req, res) => {
+    Transaction.find({ userId: req.params.userId })
+        .then(transactions => res.json(
+            transactions.map(transaction => {
+                return {
+                    type: transaction.type,
+                    description: transaction.description,
+                    value: transaction.value,
+                    id: transaction.id,
+                }
+            })
+        ))
+        .catch(err => res.status(404).json({ notransactionsfound: 'No transactions found from that user' }));
+});
+
+router.get('/:id', (req, res) => {
+    Transaction.findById(req.params.id)
+        .then(transaction => res.json(transaction))
+        .catch(err => res.status(404).json({ notransactionfound: 'No transaction found with that ID' }));
+});
+
+router.post('/new',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        const { errors, isValid } = validateTransactionInput(req.body);
+
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
+
+        const newTransaction = new Transaction({
+            type: req.body.type,
+            description: req.body.description,
+            value: req.body.value,
+            userId: req.user.id
+        });
+
+        newTransaction.save()
+            .then(transaction => res.json(transaction));
+    }
+)
+
 export default router;
