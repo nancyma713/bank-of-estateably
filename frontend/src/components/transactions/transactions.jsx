f12f0e2fbc9113682588816f89ca1d707930be01
nancyma713 <nancyma713@outlook.com>
2021-02-18 01:22:44 -0500

    render user transactions

--- frontend/src/components/transactions/transactions.jsx
+++ frontend/src/components/transactions/transactions.jsx
@@ -6,10 +6,50 @@ class Transaction extends React.Component {
         super(props);
     }
 
+    componentDidMount() {
+        this.props.fetchTransactions(this.props.currentUser.id);
+    }
+
     render() {
+        const { transactions } = this.props;
+
+        const headers = ["Category", "Description", "Value"].map((header, i) => {
+            return (
+                <th key={i}>{header}</th>
+            )
+        });
+
+        let transactionItems = transactions.map((transaction) => {
+            const intToFloat = (num, decPlaces) => num.toFixed(decPlaces);
+            const value = intToFloat(transaction.value, 2);
+            return (
+                <tr className="transaction-item" key={transaction.id}>
+                    <td>{transaction.category}</td>
+                    <td>{transaction.description}</td>
+                    <td>${value}</td>
+                </tr>
+            )
+        })
+
+        const table = <table>
+            <thead>
+                <tr>
+                    {headers}
+                </tr>
+            </thead>
+            <tbody>
+                {transactionItems}
+            </tbody>
+        </table>
+
         return (
             <div>
-                <TransactionCompose createTransaction={this.props.createTransaction} clearErrors={this.props.clearErrors}/>
+                <div className="transaction-compose">
+                    <TransactionCompose createTransaction={this.props.createTransaction} clearErrors={this.props.clearErrors} />
+                </div>
+                <div className="transaction-table">
+                    {table}
+                </div>
             </div>
         )
     }
